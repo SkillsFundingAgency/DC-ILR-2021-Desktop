1bbef38455aee99833b3b6592505bc013748f8f2
Mike Childs <mike@mjcdev.co.uk>
2019-05-09 10:45:43 +0100

    DateTimeProvider Added to DesktopContextFactory

---
+++ src/ESFA.DC.ILR.Desktop.Service.Tests/DesktopContextFactoryTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ESFA.DC.DateTimeProvider.Interface;
+using ESFA.DC.ILR.Desktop.Service.Context;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ILR.Desktop.Service.Tests
+{
+    public class DesktopContextFactoryTests
+    {
+        [Fact]
+        public void DateTimeUtc()
+        {
+            var dateTime = new DateTime(2019, 6, 1);
+
+            var dateTimeProviderMock = new Mock<IDateTimeProvider>();
+
+            dateTimeProviderMock.Setup(p => p.GetNowUtc()).Returns(dateTime);
+
+            NewFactory(dateTimeProviderMock.Object).Build("FileName").DateTimeUtc.Should().Be(dateTime);
+        }
+
+        private DesktopContextFactory NewFactory(IDateTimeProvider dateTimeProvider = null)
+        {
+            return new DesktopContextFactory(dateTimeProvider ?? Mock.Of<IDateTimeProvider>());
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.Desktop.Service/Context/DesktopContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using ESFA.DC.DateTimeProvider.Interface;
+using ESFA.DC.ILR.Desktop.Interface;
+using ESFA.DC.ILR.Desktop.Service.Interface;
+
+namespace ESFA.DC.ILR.Desktop.Service.Context
+{
+    public class DesktopContextFactory : IDesktopContextFactory
+    {
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public DesktopContextFactory(IDateTimeProvider dateTimeProvider)
+        {
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public IDesktopContext Build(string fileName)
+        {
+            return new DesktopContext()
+            {
+                DateTimeUtc = _dateTimeProvider.GetNowUtc()
+            };
+        }
+    }
+}
