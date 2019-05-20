eaadcd9c8d75a995898adf6f956f3309e4c67ab6
sharjeel <sharjeel.baloch@eas.co.uk>
2019-05-20 12:46:23 +0000

    7994 - removed unwanted namespace

--- src/ESFA.DC.ILR.Desktop.WPF.ViewModel.Tests/MainViewModelTests.cs
+++ src/ESFA.DC.ILR.Desktop.WPF.ViewModel.Tests/MainViewModelTests.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using ESFA.DC.ILR.Desktop.Service.Interface;
 using ESFA.DC.ILR.Desktop.Service.Message;
 using ESFA.DC.ILR.Desktop.WPF.Service.Interface;
-using ESFA.DC.ILR.Desktop.WPF.Views;
 using FluentAssertions;
 using Moq;
 using Xunit;
