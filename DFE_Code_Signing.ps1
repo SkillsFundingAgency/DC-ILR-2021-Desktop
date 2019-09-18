df87cb9f191ab2635cd7a002ac278a82594c15b0
Peter Chamley <pete.chamley@education.gov.uk>
2019-09-18 12:13:03 +0000

    Code Clean up.

--- DFE_Code_Signing.ps1
+++ DFE_Code_Signing.ps1
@@ -26,49 +26,36 @@ Param(
 #$AzureKeyVaultSecret=Get-AzureKeyVaultSecret -VaultName $KeyValutName.tolower().Replace(".vault.azure.net","") -Name $CertificateName -ErrorAction SilentlyContinue
 
 
-
-Write-Host  "Certificate pwdEnv has a value : $env:CODESIGNPASSWORD"; 
-Write-Host "Certificate CERT has a value : $env:CODESIGNCERT"; 
+#Write-Host  "Certificate pwdEnv has a value : $env:CODESIGNPASSWORD"; 
+#Write-Host "Certificate CERT has a value : $env:CODESIGNCERT"; 
 
 
 $cert = $env:CODESIGNCERT;
-Write-Host "Code Sign Cert DataType : $($cert.GetType().FullName)"
+#Write-Host "Code Sign Cert DataType : $($cert.GetType().FullName)"
 
-$certPwd = $env:CODESIGNPASSWORD;
-Write-Host "Code Sign Cert Pwd DataType : $($certPwd.GetType().FullName)"
+#$certPwd = $env:CODESIGNPASSWORD;
+#Write-Host "Code Sign Cert Pwd DataType : $($certPwd.GetType().FullName)"
 
 if ($null-eq$env:CODESIGNCERT)
 {   Write-Host " Certificate Error"; }
 else
 {
-
     $PrivateCertKVBytes = [System.Convert]::FromBase64String($cert)
     
-    Write-Host "a : $PrivateCertKVBytes"; 
-    Write-Host "PrivateCertKVBytes DataType : $($PrivateCertKVBytes.GetType().FullName)"
-
     $certObject = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($PrivateCertKVBytes,"");
-    #$certObject = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new("$PrivateCertKVBytes","$env:CODESIGNPASSWORD");
-    #$certObject = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new("$PrivateCertKVBytes",$null,"Exportable, PersistKeySet")
-    Write-Host "B"; 
 
     if ($null-eq$certObject)
     { Write-Host "Cert Not Found"; }
     else
     {
-        Write-Host "C"; 
-        Write-Host "Cert Thumbprint : $($certObject.Thumbprint.ToString())"
-
         $exePath = "$($StartFolder)\$($FileFilter).exe"
         $dllPath = "$($StartFolder)\$($FileFilter).dll"
 
-        Write-Debug "codeSign - EXEs"
-        Set-AuthenticodeSignature -FilePath $exePath -Certificate $certObject -TimestampServer $TimestampServer -force; # | SELECT Path | Split-Path -Leaf -Resolve
+        Write-Host "codeSign - EXEs"
+        Set-AuthenticodeSignature -FilePath $exePath -Certificate $certObject -TimestampServer $TimestampServer -force;
 
-        Write-Debug "codeSign - Dlls"
-        $DllLost = Set-AuthenticodeSignature -FilePath $dllPath -Certificate $certObject -TimestampServer $TimestampServer -force;
-
-        Write-Debug "Done"
+        Write-Host "codeSign - Dlls"
+        Set-AuthenticodeSignature -FilePath $dllPath -Certificate $certObject -TimestampServer $TimestampServer -force;
     }
 }
 
