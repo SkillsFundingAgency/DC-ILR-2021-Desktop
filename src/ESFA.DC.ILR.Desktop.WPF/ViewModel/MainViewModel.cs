903aa9738ad5394afa8d458ab60b9d515e2f1434
Mike Childs <mike@mjcdev.co.uk>
2019-11-19 12:16:07 +0000

    Fixing the mismatch of Task Exception Types

--- src/ESFA.DC.ILR.Desktop.WPF/ViewModel/MainViewModel.cs
+++ src/ESFA.DC.ILR.Desktop.WPF/ViewModel/MainViewModel.cs
@@ -207,9 +207,9 @@ namespace ESFA.DC.ILR.Desktop.WPF.ViewModel
                 CanSubmit = false;
                 FileName = _filenamePlaceholder;
             }
-            catch (OperationCanceledException operationCanceledException)
+            catch (TaskCanceledException taskCanceledException)
             {
-                _logger.LogError("Operation Cancelled", operationCanceledException);
+                _logger.LogError("Operation Cancelled", taskCanceledException);
 
                 CurrentStage = StageKeys.ChooseFile;
             }
@@ -239,6 +239,9 @@ namespace ESFA.DC.ILR.Desktop.WPF.ViewModel
                     break;
                 case ProcessingCompletionStates.UnhandledFail:
                     CurrentStage = StageKeys.ProcessUnhandledFailure;
+                    break;
+                case ProcessingCompletionStates.Cancelled:
+                    CurrentStage = StageKeys.ChooseFile;
                     break;
             }
         }
