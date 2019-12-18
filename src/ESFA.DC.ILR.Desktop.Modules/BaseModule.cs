70ba9803c1b36748110ad26f8da48619c9a40d99
Ashley Beecham <ash@momentoussolutions.co.uk>
2019-12-18 10:11:23 +0000

    91542 - refactored use of shared module registrations

---
+++ src/ESFA.DC.ILR.Desktop.Modules/BaseModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using ESFA.DC.ILR.Desktop.Utils.Modules;
+
+namespace ESFA.DC.ILR.Desktop.Modules
+{
+    public class BaseModule : Module
+    {
+        protected override void Load(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterModule<IlrServicesModule>();
+            containerBuilder.RegisterModule<IOModule>();
+            containerBuilder.RegisterModule<LoggingModule>();
+            containerBuilder.RegisterModule<SerializationModule>();
+            containerBuilder.RegisterModule<PollyModule>();
+        }
+    }
+}
