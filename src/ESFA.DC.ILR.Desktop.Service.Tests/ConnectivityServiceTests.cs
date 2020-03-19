d1ec705665bcc4a872ffa81a31e9eda0a7273e9f
Mike Childs <mike@mjcdev.co.uk>
2020-03-19 11:04:20 +0000

    SQL Server Connectivity Service

---
+++ src/ESFA.DC.ILR.Desktop.Service.Tests/ConnectivityServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Desktop.Service.Connectivity;
+using FluentAssertions;
+using Xunit;
+
+namespace ESFA.DC.ILR.Desktop.Service.Tests
+{
+    public class ConnectivityServiceTests
+    {
+        private const string ConnectionString = @"Server=(local);Integrated Security=true;";
+
+        [Fact]
+        public void GetMasterConnectionString()
+        {
+            NewService().GetMasterConnectionString(ConnectionString).Should().Be(@"Data Source=(local);Initial Catalog=Master;Integrated Security=True");
+        }
+
+        [Fact]
+        public void SQLServerTest_Invalid()
+        {
+            Action action = () => NewService().SQLServerTest("Not A Connection String");
+
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("Your Connection String is not formatted correctly.")
+                .WithInnerException<ArgumentException>();
+        }
+
+        [Fact]
+        public void SQLServerTest_ConnectionFailure()
+        {
+            Action action = () => NewService().SQLServerTest(@"Server=FictionalSqlServer;Integrated Security=true;");
+
+            action.Should().Throw<InvalidOperationException>()
+                .WithMessage("Unable to connect to the SQL Server Instance specified, check you have the correct permissions.")
+                .WithInnerException<SqlException>();
+        }
+
+        [Fact(Skip = "Not Suitable for Build Server")]
+        public void SQLServerTest_ConnectionSuccess()
+        {
+            NewService().SQLServerTest(ConnectionString).Should().BeTrue();
+        }
+
+        private ConnectivityService NewService()
+        {
+            return new ConnectivityService();
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.Desktop.Service/Connectivity/ConnectivityService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using ESFA.DC.ILR.Desktop.Service.Interface;
+
+namespace ESFA.DC.ILR.Desktop.Service.Connectivity
+{
+    public class ConnectivityService : IConnectivityService
+    {
+        private const string SelectStatement = "SELECT 1";
+
+        public bool SQLServerTest(string connectionString)
+        {
+            string masterConnectionString = null;
+
+            try
+            {
+                masterConnectionString = GetMasterConnectionString(connectionString);
+            }
+            catch (ArgumentException exception)
+            {
+                throw new ArgumentException("Your Connection String is not formatted correctly.", exception);
+            }
+
+            try
+            {
+                using (var connection = new SqlConnection(masterConnectionString))
+                {
+                    var command = new SqlCommand(SelectStatement, connection);
+
+                    connection.Open();
+                    command.ExecuteScalar();
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw new InvalidOperationException("Unable to connect to the SQL Server Instance specified, check you have the correct permissions.", exception);
+            }
+
+            return true;
+        }
+
+        public string GetMasterConnectionString(string connectionString)
+        {
+            var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString)
+            {
+                InitialCatalog = "Master"
+            };
+
+            return connectionStringBuilder.ConnectionString;
+        }
+    }
+}
