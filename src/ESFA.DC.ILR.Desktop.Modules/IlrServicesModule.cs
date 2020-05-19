c8b1b37e74324f9e430f069228c236ff722e3b30
kelvin.morris <kelvin.morris@eas.co.uk>
2020-05-19 13:55:09 +0100

    #100293 removed commented lines

--- src/ESFA.DC.ILR.Desktop.Modules/IlrServicesModule.cs
+++ src/ESFA.DC.ILR.Desktop.Modules/IlrServicesModule.cs
@@ -39,44 +39,33 @@ namespace ESFA.DC.ILR.Desktop.Modules
             containerBuilder.RegisterType<BuildDataStoreDesktopTask>().Keyed<IDesktopTask>(IlrDesktopTaskKeys.DatabaseCreate);
             containerBuilder.RegisterType<BuildDataStoreModule>().Keyed<Module>(IlrDesktopTaskKeys.DatabaseCreate);
 
-//            containerBuilder.RegisterModule<BuildDataStoreModule>();
-
             containerBuilder.RegisterType<FileValidationServiceDesktopTask>().Keyed<IDesktopTask>(IlrDesktopTaskKeys.FileValidationService);
             containerBuilder.RegisterType<FileValidationServiceDesktopModule>().Keyed<Module>(IlrDesktopTaskKeys.FileValidationService);
-//            containerBuilder.RegisterModule<FileValidationServiceDesktopModule>();
 
             containerBuilder.RegisterType<ReferenceDataServiceDesktopTask>().Keyed<IDesktopTask>(IlrDesktopTaskKeys.ReferenceDataService);
             containerBuilder.RegisterType<ReferenceDataServiceDesktopTaskModule>().Keyed<Module>(IlrDesktopTaskKeys.ReferenceDataService);
-//            containerBuilder.RegisterModule<ReferenceDataServiceDesktopTaskModule>();
 
             containerBuilder.RegisterType<ValidationServiceDesktopTask>().Keyed<IDesktopTask>(IlrDesktopTaskKeys.ValidationService);
             containerBuilder.RegisterType<ValidationServiceDesktopModule>()
                 .Keyed<Module>(IlrDesktopTaskKeys.ValidationService);
-//            containerBuilder.RegisterModule<ValidationServiceDesktopModule>();
 
             containerBuilder.RegisterType<FundingServiceDesktopTask>().Keyed<IDesktopTask>(IlrDesktopTaskKeys.FundingService);
             containerBuilder.RegisterType<FundingServiceDesktopModule>()
                 .Keyed<Module>(IlrDesktopTaskKeys.FundingService);
-//            containerBuilder.RegisterModule<FundingServiceDesktopModule>();
 
             containerBuilder.RegisterType<DataStoreDesktopTask>().Keyed<IDesktopTask>(IlrDesktopTaskKeys.DataStore);
             containerBuilder.RegisterType<DataStoreModule>().Keyed<Module>(IlrDesktopTaskKeys.DataStore);
-//            containerBuilder.RegisterModule<DataStoreModule>();
 
             containerBuilder.RegisterType<ReportServiceDesktopTask>().Keyed<IDesktopTask>(IlrDesktopTaskKeys.ReportService);
             containerBuilder.RegisterType<ReportServiceDesktopModule>().Keyed<Module>(IlrDesktopTaskKeys.ReportService);
-//            containerBuilder.RegisterModule<ReportServiceDesktopModule>();
 
             containerBuilder.RegisterType<PostProcessingDesktopTask>().Keyed<IDesktopTask>(IlrDesktopTaskKeys.PostExecution);
 
             containerBuilder.RegisterType<BuildMdbDesktopTask>().Keyed<IDesktopTask>(IlrDesktopTaskKeys.MdbCreate);
             containerBuilder.RegisterType<BuildMdbModule>().Keyed<Module>(IlrDesktopTaskKeys.MdbCreate);
-//            containerBuilder.RegisterModule<BuildMdbModule>();
 
             containerBuilder.RegisterType<MdbDesktopTask>().Keyed<IDesktopTask>(IlrDesktopTaskKeys.MdbExport);
             containerBuilder.RegisterType<MdbModule>().Keyed<Module>(IlrDesktopTaskKeys.MdbExport);
-
-//            containerBuilder.RegisterModule<MdbModule>();
 
             containerBuilder.RegisterType<PublishMdbDesktopTask>().Keyed<IDesktopTask>(IlrDesktopTaskKeys.MdbPublish);
 
